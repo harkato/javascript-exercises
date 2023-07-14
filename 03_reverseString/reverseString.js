08622a7c911fbd1c89687ebf757f5c0fcd24e1f7
harkato <92404938+harkato@users.noreply.github.com>
2023-07-14 18:38:21 -0300

    Solve third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverseStringArray = [];
+    for (let i = 0; i<string.length; i++) {
+        reverseStringArray[string.length-1-i] = string.charAt(i);
+    }
+    let reversedString = reverseStringArray.join("");
+    return reversedString;
 };
 
 // Do not edit below this line
