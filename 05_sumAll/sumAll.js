c175ad5dd77fea714f9cd6324e95a821bf62d686
harkato <92404938+harkato@users.noreply.github.com>
2023-07-14 20:51:54 -0300

    Solve fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(initialNumber,finalNumber) {
+    finalResult = 0;
+    let times = Math.abs(finalNumber-initialNumber);
+    let i = 0;
+    
+    if (!Number.isInteger(initialNumber) || !Number.isInteger(finalNumber) || initialNumber<0 || finalNumber<0  ) {
+        return "ERROR";
+    } else if (initialNumber>finalNumber){
+        while(i<=times){
+            finalResult += finalNumber;
+            finalNumber++;
+            i++;
+        }
+    } else {
+        while(i<=times){
+            finalResult += initialNumber;
+            initialNumber++;
+            i++;
+        }
+    }
 
+    return finalResult;
 };
 
 // Do not edit below this line
