582b665db4f024f36ba34204c32acb8551b3ab36
harkato <92404938+harkato@users.noreply.github.com>
2023-07-13 22:43:50 -0300

    Solve second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let i = 0;
+    let result = "";
+    if (num >= 0) {
+        while (i<num){
+            if (num>0){
+                result += string;
+                i++;
+            }
+        }
+        return result;
+    } else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
