0f9d1417a23e5e5d070501a053f71b6dce7623fe
harkato <92404938+harkato@users.noreply.github.com>
2023-07-14 19:00:15 -0300

    Solve fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,argument) {
+    const args = Array.from(arguments);
+    for (let j = 0; j < args.length; j++) {
+        argument = args[j];
+        for (let i = 0; i < array.length; i++) {
+            if (argument === array[i]) {
+                array.splice(i,1);
+            }
+        }
+    }
+    
+    return array;
 };
 
 // Do not edit below this line
