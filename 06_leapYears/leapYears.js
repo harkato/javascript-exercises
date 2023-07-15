8b25dde9293e1ada4dcbc40aa44c2b91e211a017
harkato <92404938+harkato@users.noreply.github.com>
2023-07-14 21:01:15 -0300

    Solve sixth exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    firstCondition = year % 4 == 0;
+    secondCondition = year % 100 == 0;
+    thirdCondition = year % 400 == 0;
+    if (firstCondition && thirdCondition && secondCondition) {
+        return true;
+    } else if (firstCondition && !secondCondition && !thirdCondition){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
