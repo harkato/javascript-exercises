6bc8746bda857a5dc98d59f10088ed0d74719354
harkato <92404938+harkato@users.noreply.github.com>
2023-07-14 21:27:01 -0300

    Solve seventh exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureFahrenheit) {
+  let result = 0;
+  let answer = 0;
+  result = ((temperatureFahrenheit-32) *5/9)
+  if (!Number.isInteger(result)) {
+    answer = Number(((result)).toFixed(1));
+    return answer;
+  }
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureCelsius) {
+  let result = 0;
+  let answer = 0;
+  result = ((temperatureCelsius*9/5) +32);
+  if (!Number.isInteger(result) ) {
+    answer = Number(((result)).toFixed(1));
+    return answer;
+  }
+  return result;
 };
 
 // Do not edit below this line
