f9f308658c61f15861b1ad0051ece7dec13ec07b
harkato <92404938+harkato@users.noreply.github.com>
2023-08-10 18:02:45 -0300

    Solve 8th exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,70 @@
-const add = function() {
-	
+const add = function(firstNumber, secondNumber) {
+  let numbers = {
+    firstNumber,
+    secondNumber,
+  }
+  let result = 0;
+  for(number in numbers) {
+    result += numbers[number]
+  }
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNumber, secondNumber) {
+  let numbers = {
+    firstNumber,
+    secondNumber,
+  }
+  return numbers.firstNumber - numbers.secondNumber;
 };
 
-const sum = function() {
-	
+const sum = function(...args) {
+    if (Array.isArray(args[0])) {
+      args = args[0];
+    }
+    const numbers = {};
+    for (let i = 0; i < args.length; i++) {
+        const key = `number${i + 1}`;
+        numbers[key] = Number(args[i]);
+    }
+    let sum = 0;
+    for (const key in numbers) {
+      sum+= numbers[key];
+    }
+    return sum;
+}
+
+const multiply = function(...args) {
+  let numbers = {}
+    for (let i = 0; i < args.length; i++) {
+      const key = `number${i + 1}`;
+      numbers[key] = Number(args[i]);
+  }
+  let multi = 1;
+  for (const key in numbers) {
+    multi*= numbers[key];
+  }
+  return multi;
+}
+
+const power = function(firstNumber, secondNumber) {
+  let numbers = {
+    firstNumber,
+    secondNumber,
+  }
+  return numbers.firstNumber ** numbers.secondNumber;
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (!(number === 0)) {
+    let result = 1;
+    for (let i = number; i>0; i--) {
+      result*= i;
+    }
+    return result;
+  } else {
+    return 1;
+  }
 };
 
 // Do not edit below this line
