801c13f515bf05633f06d7a0ed05ae3c0b658cd6
harkato <92404938+harkato@users.noreply.github.com>
2023-08-10 18:26:58 -0300

    Solve first test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function isPalindrome(string) {
+    reverseString = string.split('').reverse().join('').toLowerCase();
+    originalString = string.toLowerCase();
+    if(originalString == reverseString) return true;
+    else return false;
 };
 
 // Do not edit below this line
