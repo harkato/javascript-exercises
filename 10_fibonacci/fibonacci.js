66f04cf410aa9a289123de6c768081405e2abe9a
harkato <92404938+harkato@users.noreply.github.com>
2023-08-13 19:46:34 -0300

    Solve 10th exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number < 0) return "OOPS";
+    number = parseInt(number);
+    if (number <= 2) return 1;
+    let sum = 0;
+    let firstNumber = 1;
+    let secondNumber = 1;
+    for (let i = number-2; i>0; i--) {
+        sum = firstNumber + secondNumber;
+        firstNumber = secondNumber;
+        secondNumber = sum;
+    }
+    return sum;
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+
+// 1, 1, 2, 3, 5, 8
